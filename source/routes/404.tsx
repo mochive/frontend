5e40ff33599aec5c44ad34d56c4fa30b311c0676
H2Owater425 <h2o@h2owr.xyz>
2023-12-27 12:41:25 +0900

    feat: Fix unit to vh

--- source/routes/404.tsx
+++ source/routes/404.tsx
@@ -1,7 +1,7 @@
 import { JSXNode, component$, useStyles$ } from '@builder.io/qwik';
 
 export default component$(function (): JSXNode {
-	useStyles$('body,div{display:flex}body{background-color:#e6e6e6;justify-content:center;align-items:center;height:100lvh}div{border:1px solid #949494;width:100%;max-width:700px;height:150px;justify-content:center;align-items:center;border-radius:16px;flex-direction:column;gap:4px}a,h1{font-weight:400;font-family:\'굴림\',Gulim}h1{font-size:2.4rem}@media (width <= 840px){div{width:90%}h1{font-size:2.2rem}a{font-size:.9rem}}@media (width <= 490px){div{width:95%}h1{font-size:2rem}a{font-size:.8rem}}');
+	useStyles$('body,div{display:flex}body{background-color:#e6e6e6;justify-content:center;align-items:center;height:100vh}div{border:1px solid #949494;width:100%;max-width:700px;height:150px;justify-content:center;align-items:center;border-radius:16px;flex-direction:column;gap:4px}a,h1{font-weight:400;font-family:\'굴림\',Gulim}h1{font-size:2.4rem}@media (width <= 840px){div{width:90%}h1{font-size:2.2rem}a{font-size:.9rem}}@media (width <= 490px){div{width:95%}h1{font-size:2rem}a{font-size:.8rem}}');
 	
 	return <div>
 		<h1>이 면은 여백입니다.</h1>
